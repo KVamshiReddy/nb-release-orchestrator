ed6d8e051b8fb91dd3b7b70157ba861e65b55742
Vamshi_3103 <vkovvuri@stevens.edu>
2026-02-20 09:39:25 -0500

    Initial Commit

---
+++ src/main/java/com/jira_client/JiraClientApplication.java
@@ -0,0 +1,13 @@
+package com.jira_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraClientApplication.class, args);
+	}
+
+}
