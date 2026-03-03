062d08704c0100a1254e0472fcaf1ba8c0585b2d
Vamshi_3103 <vkovvuri@stevens.edu>
2026-03-03 17:29:26 -0500

    1. Added JiraController file with the endpoint to get only the required fields from the API. 2. Tested for functionality.

--- src/main/java/com/jira_client/service/JiraService.java
+++ src/main/java/com/jira_client/service/JiraService.java
@@ -1,6 +1,11 @@
 package com.jira_client.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jira_client.config.JiraConfig;
+import com.jira_client.model.Assignee;
+import com.jira_client.model.Issue;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +42,7 @@ public class JiraService {
         return headers;
     }
 
-    public String getRawIssue(String key) {
+    public Issue getRawIssue(String key) {
         if (key.trim().isEmpty()) {
             throw new IllegalArgumentException("A valid key must be passed");
         }
@@ -52,11 +57,32 @@ public class JiraService {
                     String.class   // the response type you want back
             );
             logger.info("Successfully fetched issue: {}", key);
-            return response.getBody();
+            return getCleanIssue(response.getBody());
         } catch (Exception e) {
             logger.error("Error fetching issue {}: {}", key, e.getMessage());
             throw  new RuntimeException("Issue Fetching Failed");
         }
+    }
+
+    public Issue getCleanIssue(String rawIssue) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode data = mapper.readTree(rawIssue);
+        Issue issue = new Issue();
+        issue.setIssueType(data.get("fields").get("issuetype").get("name").asText());
+        issue.setId(data.get("id").asText());
+        issue.setKey(data.get("key").asText());
+        issue.setSummary(data.get("fields").get("summary").asText());
+        issue.setStatus(data.get("fields").get("statusCategory").get("name").asText());
+        issue.setPriority(data.get("fields").get("priority").get("name").asText());
+
+        Assignee assignee = new Assignee();
+        assignee.setDisplayName(data.get("fields").get("assignee").get("displayName").asText());
+        assignee.setAccountId(data.get("fields").get("assignee").get("accountId").asText());
+        assignee.setEmail(data.get("fields").get("assignee").get("emailAddress").asText());
+
+        issue.setAssignee(assignee);
+
+        return issue;
     }
 
 }
